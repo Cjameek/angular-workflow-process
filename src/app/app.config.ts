8250d968e88442ab40a1fcf586ae5d2a46975d61
Cjameek <cody.meek01@gmail.com>
2025-10-22 10:57:27 -0400

    feat: add custom plugin for submit.prevent event

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,11 +2,18 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessC
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { EVENT_MANAGER_PLUGINS } from '@angular/platform-browser';
+import { PreventDefaultEventPlugin } from './prevent-default-event-plugin';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
-    provideRouter(routes)
+    provideRouter(routes),
+    {
+      provide: EVENT_MANAGER_PLUGINS,
+      multi: true,
+      useClass: PreventDefaultEventPlugin
+    }
   ]
 };
---
+++ src/app/prevent-default-event-plugin.ts
@@ -0,0 +1,20 @@
+import { ListenerOptions } from "@angular/core";
+import { EventManagerPlugin } from "@angular/platform-browser"
+
+export class PreventDefaultEventPlugin extends EventManagerPlugin {
+  override supports(eventName: string): boolean {
+    return eventName.endsWith('.prevent');
+  }
+  override addEventListener(element: HTMLElement, eventName: string, handler: Function, options?: ListenerOptions): Function {
+    const originalEvent = eventName.split('.')[0];
+    return this.manager.addEventListener(
+      element,
+      originalEvent,
+      (e: Event) => {
+        e.preventDefault();
+        handler(e);
+      }
+    )
+  }
+
+}
