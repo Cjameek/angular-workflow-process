1e571e2480c796aee8241aa5305268880c3d69c0
Cjameek <cody.meek01@gmail.com>
2025-10-23 21:46:50 -0400

    feat(options-menu): change icon and hover style

--- src/app/approval-workflow/ui/workflow-options-menu/workflow-options-menu.ts
+++ src/app/approval-workflow/ui/workflow-options-menu/workflow-options-menu.ts
@@ -5,11 +5,15 @@ import { CdkMenuModule } from '@angular/cdk/menu';
   selector: 'workflow-options-menu',
   template: `
     <button 
-      class="inline-flex items-center justify-center bg-gray-200 w-8 h-8 rounded-full cursor-pointer" 
+      class="inline-flex items-center justify-center hover:bg-gray-100 w-8 h-8 rounded-full cursor-pointer transition-colors" 
       type="button" 
       [cdkMenuTriggerFor]="workflowOptions"
     >
-        <span>...</span>
+        <span class="text-gray-500 inline-flex h-[1em]">
+          <svg aria-hidden="true" focusable="false" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 128 512">
+            <path fill="currentColor" d="M64 360a56 56 0 1 0 0 112 56 56 0 1 0 0-112zm0-160a56 56 0 1 0 0 112 56 56 0 1 0 0-112zM120 96A56 56 0 1 0 8 96a56 56 0 1 0 112 0z"></path>
+          </svg>
+        </span>
     </button>
 
     <ng-template #workflowOptions>
